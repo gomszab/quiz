d2eb5f21344d8770bd52423ee7197f7fd42365fb
gomszab <gombosszabolcs88@gmail.com>
2025-02-16 20:06:00 +0100

    add manager to area classes

--- app/area.js
+++ app/area.js
@@ -5,11 +5,18 @@ class Area{
         return this.#div;
     }
 
-    constructor(cssClass){
+    constructor(cssClass, manager){
         const container = this.#getContainer();
         this.#div = document.createElement('div');
         this.#div.className = cssClass;
         container.appendChild(this.#div);
+        manager.setFinishCallback((resultText) => {
+            container.innerHTML = '';
+            const resultDiv = document.createElement('div');
+            resultDiv.className = 'result';
+            resultDiv.textContent = resultText
+            container.appendChild(resultDiv);
+        })
     }
 
     #getContainer(){
@@ -24,13 +31,32 @@ class Area{
 }
 
 class AnswersArea extends Area{
-    constructor(cssClass){
-        super(cssClass)
+    constructor(cssClass, manager){
+        super(cssClass, manager)
+
+        manager.setNextAnswersCallback((answers) => {
+            this.div.innerHTML = '';
+            for(const answer of answers){
+                const answerCard = document.createElement('div');
+                answerCard.className = 'item';
+                answerCard.textContent = answer;
+                answerCard.addEventListener('click', () => {
+                    manager.nextQuestion(answer);
+                })
+                this.div.appendChild(answerCard);
+            }
+        })
     }
 }
 
 class QuestionArea extends Area{
-    constructor(cssClass){
-        super(cssClass)
+    constructor(cssClass, manager){
+        super(cssClass, manager)
+        manager.setNextQuestionCallback((questionText) => {
+            this.div.innerHTML = '';
+            const questionCard = document.createElement('div');
+            questionCard.textContent = questionText;
+            this.div.appendChild(questionCard);
+        })
     }
 }
