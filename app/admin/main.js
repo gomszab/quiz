38abfdd23733493183bb093c7d11bd485617a9b2
gomszab <gombosszabolcs88@gmail.com>
2025-03-08 14:39:15 +0100

    add form to admin

--- app/admin/main.js
+++ app/admin/main.js
@@ -6,3 +6,6 @@ const formFields = [
     { id: 'answer4', label: 'valasz4' },
     { id: 'rightAnswer', label: 'helyes valasz' },
   ];
+
+  const manager = new Manager();
+  const form = new FormController(manager,formFields);
